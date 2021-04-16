6935afe86750c8c69035494f80324a8c1e5b45b8
Miguel Alcantar <malcantar185@gmail.com>
2021-04-15 21:14:30 -0700

    routes worked

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,61 @@
-const db = require("../models");
-const router = require("express").Router(); 
-
-
-  router.get("/api/workouts", function(req, res) {
-    db.Workout.find({}).then(function(dbWorkouts) {
-      res.json(dbWorkouts);
-    });
-  });
-
-  router.put("/api/workouts/:id", function(req, res) {
-    db.Workout.updateOne({ _id: req.params.id }, { rating: req.body.rating }).then(function(dbWorkouts) {
-      res.json(dbWorkouts);
-    });
-  });
+const router = require("express").Router();
+const db = require("../Models");
 
 
 
-module.exports = router; 
+//create new workout
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//add exercises
+router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body.duration);
+    db.Workout.findOneAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+            
+        },
+        {
+            new: true,
+            upsert: true
+        }
+    ).then(updatedWorkout => {
+        console.log(updatedWorkout);
+        res.json(updatedWorkout);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
